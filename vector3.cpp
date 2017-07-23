0b2fbe85b881d59f9a67c893933f9915a689e101
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-07-23 18:02:36 +0200

    Checkin time.

--- vector3.cpp
+++ vector3.cpp
@@ -2,12 +2,13 @@
 
 int main() {
     std::vector<double> w {2,3,4};
-    Vector3 v(1,2,3);
-    Vector3 v0;
-    v = Vector3(w);
+    Vector3 v1(1,2,3);
+    Vector3 v2(2,3,4);
+    Vector3 v3(3,4,5);
+    Vector3 v = v1 + v2 + v3;
     w = std::vector<double>(v);
     
-    bool t = (v==v0);
+    bool t = (v==v1);
 
     std::cout << t << std::endl;
 }
