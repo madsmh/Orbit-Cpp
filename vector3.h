94d6398cae745e006f704333c1d8256d249c8881
Mads M. Hansen <madsmh@gmail.com>
2017-07-23 15:36:25 +0200

    Implemented commutative scalar multiplication.

--- vector3.h
+++ vector3.h
@@ -1,20 +1,26 @@
+#ifndef ORBIT3D_VECTOR3_H
+#define ORBIT3D_VECTOR3_H
+
+
 #include <vector>
 #include <iostream>
-#include "math.h"
+#include "cmath"
 
 class Vector3 {
     double m_x, m_y, m_z;
 public:
-    Vector3(double x = 0.0, double y = 0.0, double z = 0.0) 
-        : m_x(x), m_y(y), m_z(z) {}
-    Vector3(std::vector<double> v) : m_x(v[0]), m_y(v[1]), m_z(v[2]) {}
+    explicit Vector3(double x = 0.0, double y = 0.0, double z = 0.0)
+            : m_x(x), m_y(y), m_z(z) {}
+
+    explicit Vector3(std::vector<double> v) : m_x(v[0]), m_y(v[1]), m_z(v[2]) {}
     double norm2() const { return m_x*m_x + m_y*m_y + m_z*m_z;}
     double norm()  const { return sqrt(norm2());}
     double x()     const { return m_x;}
     double y()     const { return m_y;}
     double z()     const { return m_z;}
-    operator std::vector<double>() const  { return std::vector<double> {m_x, m_y, m_z}; }
-    
+
+    explicit operator std::vector<double>() const  { return std::vector<double> {m_x, m_y, m_z}; }
+
     Vector3& operator += (const Vector3& rhs) {
         m_x += rhs.m_x;
         m_y += rhs.m_y;
@@ -29,6 +35,20 @@ public:
         return *this;
     }
 
+    Vector3&operator *= (const double rhs) {
+        m_x *= rhs
+        m_y *= rhs;
+        m_z *= rhs;
+    }
+    friend Vector3 operator * (Vector3& lhs, const double rhs ){
+        return lhs *= rhs;
+    }
+
+    friend Vector3 operator * (const double lhs, Vector3& rhs ){
+        return rhs *= lhs;
+    }
+
+
     friend Vector3 operator + (Vector3& lhs, const Vector3& rhs) {
         return lhs += rhs;
     }
@@ -42,3 +62,4 @@ public:
     }
 };
 
+#endif //ORBIT3D_VECTOR3_H
