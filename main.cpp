544d8bacb75823e60bf9644ac629c267b815058b
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-07-23 12:35:33 +0200

    Fixed som bugs in file handling.

--- main.cpp
+++ main.cpp
@@ -407,10 +407,12 @@ public:
             while (!filereader.eof()){
                 getline(filereader, buffer, ',');
                 p.name = buffer;
+                std::cout << "Reading data for object " << buffer << std::endl;
                 getline(filereader, buffer, ',');
                 p.GM = stod(buffer);
                 getline(filereader, buffer, ',');
                 p.r = stod(buffer);
+                getline(filereader, buffer, '\n');
 
                 data.emplace_back(p);
             }
