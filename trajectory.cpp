1ed989356d945ebade65be8b9097f7a0f99cfef1
Mads M. Hansen <madsmh@gmail.com>
2017-07-24 12:58:14 +0200

    Made the code a bit more understandable.

--- trajectory.cpp
+++ trajectory.cpp
@@ -6,47 +6,47 @@
 
 Trajectory::Trajectory(long n_trajectories, long n_rows) {
 
-    for (int i = 0; i < n_trajectories; ++i) {
+    self_n_trajectories = n_trajectories;
+
+    for (unsigned int i = 0; i < self_n_trajectories; ++i) {
         self_positions.emplace_back(std::vector<Vector3> {});
         self_velocities.emplace_back(std::vector<Vector3> {});
     }
-
-    self_n_trajectories = n_trajectories;
     self_n_rows = n_rows;
 }
 
-void Trajectory::set_position(std::vector<Vector3> pos,
-                              std::vector<Vector3> vel){
-    for (int i = 0; i< self_n_trajectories ; ++i) {
+void Trajectory::set_position(std::vector<Vector3> pos, std::vector<Vector3> vel){
+    for (unsigned int i = 0; i< self_n_trajectories ; ++i) {
         self_positions[i].emplace_back(pos[i]);
         self_velocities[i].emplace_back(vel[i]);
     }
 }
 
-std::vector<Vector3> Trajectory::get_trajectory_positions(long i){
-    return self_positions[i];
+std::vector<Vector3> Trajectory::get_trajectory_positions(int body){
+    std::cout << self_positions[body].size() << "\n";
+    return self_positions[body];
 }
 
-std::vector<Vector3> Trajectory::get_trajectory_velocities(long i) {
-    return self_velocities[i];
+std::vector<Vector3> Trajectory::get_trajectory_velocities(int body) {
+    return self_velocities[body];
 }
-std::vector<Vector3 > Trajectory::get_positions_at_index(int i){
+std::vector<Vector3 > Trajectory::get_positions_at_index(int index){
 
     std::vector<Vector3 > positions;
 
     for (int j = 0; j < self_n_trajectories; ++j) {
-        positions.emplace_back(self_positions[j][i]);
+        positions.emplace_back(self_positions[j][index]);
     }
 
     return positions;
 }
 
-std::vector<Vector3> Trajectory::get_velocities_at_index(int i) {
+std::vector<Vector3> Trajectory::get_velocities_at_index(int index) {
 
     std::vector<Vector3> velocities;
 
     for (int j = 0; j < self_n_trajectories; ++j) {
-        velocities.emplace_back(self_velocities[j][i]);
+        velocities.emplace_back(self_velocities[j][index]);
     }
 
     return velocities;
--- trajectory.h
+++ trajectory.h
@@ -6,30 +6,29 @@
 #define ORBIT3D_TRAJECTORY_H
 
 #include "vector3.h"
+#include <vector>
 
 class Trajectory{
 
     long self_n_rows;
     long self_n_trajectories;
-    std::vector<std::vector<Vector3 > > self_positions;
-    std::vector<std::vector<Vector3 > > self_velocities;
+    std::vector<std::vector<Vector3>> self_positions;
+    std::vector<std::vector<Vector3>>  self_velocities;
 
 public:
     Trajectory(long n_trajectories, long n_rows);
 
-    void set_position(std::vector<Vector3>, std::vector<Vector3>);
+    void set_position(std::vector<Vector3> pos, std::vector<Vector3> vel);
 
-    std::vector<Vector3> get_trajectory_positions(long i);
+    std::vector<Vector3> get_trajectory_positions(int body);
 
-    std::vector<Vector3> get_trajectory_velocities(long i);
+    std::vector<Vector3> get_trajectory_velocities(int body);
 
-    std::vector<Vector3 > get_positions_at_index(int);
+    std::vector<Vector3 > get_positions_at_index(int index);
 
-    std::vector<Vector3 > get_velocities_at_index(int);
+    std::vector<Vector3 > get_velocities_at_index(int index);
 
-    long get_number_of_rows(){
-        return self_n_rows;
-    }
+    long get_number_of_rows(){ return self_n_rows; }
 };
 
 
