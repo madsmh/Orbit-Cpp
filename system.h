93c56364031ec8b4f38319174ce86e708e294183
Mads M. Hansen <madsmh@gmail.com>
2017-07-24 21:40:13 +0200

    Added copyright notice.

--- system.h
+++ system.h
@@ -1,6 +1,22 @@
-//
-// Created by mads on 23-07-17.
-//
+/* Copyright (c) 2017 By the Authors
+ *
+ * This file is part of Orbit Simulator.
+
+    Orbit Simulator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Orbit Simulator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Orbit Simulator.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
+
 
 #ifndef ORBIT3D_SYSTEM_H
 #define ORBIT3D_SYSTEM_H
