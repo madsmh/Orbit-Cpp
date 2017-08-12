5e4fb77ffaca93bd74875352988a5df995cff0ce
Mads M. Hansen <madsmh@gmail.com>
2017-08-12 15:22:34 +0200

    Copyright notice.

--- importdata.h
+++ importdata.h
@@ -1,3 +1,22 @@
+/* Copyright (c) 2017 By the Authors
+ *
+ * This file is part of Orbit Simulator.
+
+    Orbit Simulator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Orbit Simulator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
+
 #ifndef IMPORTDATA_H
 #define IMPORTDATA_H
 
