6785edba3aaf3ceafc2b19ad59e86b55c912c744
Mads M. Hansen <madsmh@gmail.com>
2018-02-02 16:29:37 +0100

    Updated diagnostic files.

--- diagnostics/anim_pos.py
+++ diagnostics/anim_pos.py
@@ -10,9 +10,8 @@ data_sim_pos = np.genfromtxt('pos_sim.csv', delimiter=', ')*1e-3
 
 def animate(i, data, lines):
     for dat, line in zip(data, lines):
-        line[0].set_data(dat[:i,0], dat[:i, 1])
-        line[0].set_3d_properties(dat[:i,2])
-
+        line[0].set_data(dat[i-10:i,0], dat[i-10:i, 1])
+        line[0].set_3d_properties(dat[i-10:i,2])
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -25,10 +24,10 @@ ax.legend()
 # ax.relim()
 # ax.autoscale_view(True, True, True)
 
-anim = animation.FuncAnimation(fig, animate, fargs=(data, lines),frames=300, interval=900)
+anim = animation.FuncAnimation(fig, animate, fargs=(data, lines),frames=data_sim_pos.shape[0], interval=100, repeat=False)
 
 
 
-anim.save('test.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+# anim.save('test.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
 plt.show()
---
+++ diagnostics/plot_velocity.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data_angles = np.genfromtxt('angles.csv', delimiter=', ')
+data_error = np.genfromtxt('error_pos.csv', delimiter=', ')
